b858e3aab4c09bdd9dc0190ec904675ba9da9deb
IrvingGuerra <37481550+IrvingGuerra@users.noreply.github.com>
2020-02-02 21:30:30 -0600

    Node
    js

--- index.js
+++ index.js
@@ -1,27 +1,17 @@
-"use strict";
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
-app.post("/sacvog", function(req, res) {
-  console.log("[OK] Se recibio una peticion Post");
-  respuesta = "Que onda, que pex";
-  return res.json({
-      fulfillmentText: respuesta,
-      source: "webhook-echo-sample"
-  });
-
+app.post('/clima', function (req, res) {
+  res.json({fulfillmentText: 'Respondientdo desde webhook en node'});
 });
 
-app.listen(process.env.PORT || 8000, function() {
-  console.log("Server up and listening in port 8000");
+app.listen(3000, function () {
+  console.log('App escuchando puerto 3000');
 });
-
-
 
 
 
