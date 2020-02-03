6a6a60ae72fbab9d433b93dae9353f3a080cb340
IrvingGuerra <37481550+IrvingGuerra@users.noreply.github.com>
2020-02-02 20:35:13 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+"use strict";
+const express = require("express");
+const bodyParser = require("body-parser");
+const restService = express();
+
+restService.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+restService.use(bodyParser.json());
+// Funcion que se llama desde DialogFlow
+restService.post("/echo", function(req, res) {
+  /*
+  var Variable1 =
+    req.body.queryResult &&
+    req.body.queryResult.parameters &&
+    req.body.queryResult.parameters.Sensores
+      ? req.body.queryResult.parameters.Sensores
+      : "vacio";
+  */
+
+  respuesta = "Que onda, que pex";
+  return res.json({
+      fulfillmentText: respuesta,
+      source: "webhook-echo-sample"
+  });
+
+});
+
+restService.listen(process.env.PORT || 8000, function() {
+  console.log("Server up and listening");
+});
+
+
+
+
+
+
